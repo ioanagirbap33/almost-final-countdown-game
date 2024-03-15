69de83cd452aaa3e973c32c93b08d28af5a8d914
ioana.girba@principal33.com <ioana.girba@ITRO-166.local>
2024-03-15 20:43:59 +0200

    add details about timing score in modal

--- almost-final-countdown/src/components/ResultModal.jsx
+++ almost-final-countdown/src/components/ResultModal.jsx
@@ -1,19 +1,31 @@
-import { forwardRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 
 const ResultModal = forwardRef(function ResultModal(
-  { result, targetTime },
+  { targetTime, timeRemaining, onReset },
   ref
 ) {
+  const dialog = useRef();
+
+  const userLost = timeRemaining <= 0;
+  const formattedRemainingTime = (timeRemaining / 1000).toFixed(2);
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
   return (
-    <dialog ref={ref} className="result-modal" open>
-      <h2>You {result}</h2>
+    <dialog ref={dialog} className="result-modal">
+      {userLost && <h2>You lost</h2>}
       <p>
         The target time was <strong>{targetTime} seconds</strong>
       </p>
       <p>
-        You stoped timer with <strong>X seconds left</strong>
+        You stoped timer with{" "}
+        <strong>{formattedRemainingTime} seconds left</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
