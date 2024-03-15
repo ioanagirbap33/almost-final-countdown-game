2f2dc469c6f6ed8edd989ff00ae6607f07c8953f
ioana.girba@principal33.com <ioana.girba@ITRO-166.local>
2024-03-15 21:02:38 +0200

    add portal

--- almost-final-countdown/src/components/ResultModal.jsx
+++ almost-final-countdown/src/components/ResultModal.jsx
@@ -1,5 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
-
+import { createPortal } from "react-dom";
 const ResultModal = forwardRef(function ResultModal(
   { targetTime, timeRemaining, onReset },
   ref
@@ -17,7 +17,7 @@ const ResultModal = forwardRef(function ResultModal(
       },
     };
   });
-  return (
+  return createPortal(
     <dialog ref={dialog} className="result-modal">
       {userLost && <h2>You lost</h2>}
       {!userLost && <h2>Your score: {score}</h2>}
@@ -32,7 +32,9 @@ const ResultModal = forwardRef(function ResultModal(
       <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+
+    document.getElementById("modal")
   );
 });
 
